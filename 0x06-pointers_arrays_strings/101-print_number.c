db46ea2266d75df8b9aecd2a88ca6357cce406e7
Owala <40026791+Owala1@users.noreply.github.com>
2022-12-22 01:45:49 +0100

    printf update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,52 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- *print_number - prints an integer
- *
- *@n:integer params
- *
- *Return: 0
- *
+ * base10 - power in 10 base
+ * @n: an exponent
+ * Return: returns 10 to power exponent
+ */
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
+
+/**
+ * print_number - prints integers enters as parameters using putchar
+ * @n: integer to print
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int x;
+	int power;
 
-	x = n;
+	power = base10(8);
+
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -n;
+		n *= -1;
 	}
-	if (x / 10 != 0)
+
+	if (n == 0)
+		_putchar('0');
+
+	else
 	{
-		print_number(x / 10);
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
 	}
-	_putchar((x % 10) + '0');
 }
